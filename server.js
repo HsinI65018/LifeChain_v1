6ae022cab0c5c171beaf7b7dbb61c76bf34c2a4c
nancy <nancy65018@gmail.com>
2022-08-10 21:43:04 +0800

    [create]:initialize repo

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const render = require('./router/render');
+
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use('/', render);
+
+const PORT = '3000';
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST);
+console.log('server listening at port 3000...');
